255ed8047e6e51ad5baf7a7325b5930418f31c74
carlo <>
2021-09-29 17:06:03 +0800

    添加openid

--- mp/oauth2/session.go
+++ mp/oauth2/session.go
@@ -18,7 +18,7 @@ type Session struct {
 }
 
 type SessionInfo struct {
-	OpenId   string `json:"openid"`   // 用户的唯一标识
+	OpenId   string `json:"openId"`   // 用户的唯一标识
 	Nickname string `json:"nickName"` // 用户昵称
 	Gender   int    `json:"gender"`   // 用户的性别, 值为1时是男性, 值为2时是女性, 值为0时是未知
 	Language string `json:"language"` // 用户的语言
@@ -32,7 +32,9 @@ type SessionInfo struct {
 	// 用户头像，最后一个数值代表正方形头像大小（有0、46、64、96、132数值可选，0代表640*640正方形头像），
 	// 用户没有头像时该项为空。若用户更换头像，原有头像URL将失效。
 	AvatarUrl string `json:"avatarUrl"`
-	UnionId   string `json:"unionid"` // 只有在将小程序绑定到微信开放平台帐号后，才会出现该字段。
+	UnionId   string `json:"unionId"` // 只有在将小程序绑定到微信开放平台帐号后，才会出现该字段。
+	OpenID     string `json:"openid"`
+	UnionID    string `json:"unionid"`
 }
 
 type WaterMark struct {
